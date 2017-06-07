7e1a2aa73e748c8a1946de5b03def9c698ff89c7
grabowskipawel <grabowskipawel91@gmail.com>
2017-06-07 12:42:55 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
     
-  /* $(window).resize(function(){ // check window size, and add proper animation
-       var windowWidth = window.innerWidth;
+       var $animation_elements = $('#one');
+var $window = $(window);
+   
+    TweenLite.to($(".short"), 1.0 , {opacity:1});
+    TweenLite.to($(".firstShow"),1.0,{opacity:1 , delay:1})
+     TweenLite.to($(".secondShow"),1.0,{opacity:1 , delay:1.5})
+      TweenMax.to($(".short::before"), 1.0 , {opacity:1});
+      TweenMax.to($(".short::after"), 1.0 , {opacity:1});
+    TweenMax.to($(".thirdShow") , 1.0 ,{opacity:1 , delay:2})
+    
+           var windowWidth = window.innerWidth;
+    
+  
+    
+    $(window).innerWidth( function(){ // check window size, and add proper animation
+
     
          if(windowWidth >= 850){
-             alert("Mam 850 px lub więcej")
-         }else if(windowWidth <=850){
-             alert("Mam mniej niż 850px")
-         }
-     
-    
-   });*/
-     
-    
-     var $animation_elements = $('#one');
-var $window = $(window);
-     
-   function check_if_in_view() {
+             
+              function check_if_in_view() {
   var window_height = $window.height();
   var window_top_position = $window.scrollTop();
   var window_bottom_position = (window_top_position + window_height);
@@ -31,20 +34,19 @@ var $window = $(window);
     if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
               
-    
-        
-          TweenLite.to($("#two"), 1.5, { ease: Bounce.easeOut, x:-475});
+    TweenLite.to("#contact", 1, {x:0 , opacity:1});
+         TweenLite.to($(".example") ,1, {opacity:1, delay:2.0});
+          TweenLite.to($("#two"), 2.0, { ease: Bounce.easeOut, x:-475 , opacity:1});
          
-          TweenLite.to($("#one"), 1.5, { ease: Bounce.easeOut, x:150 });
+          TweenLite.to($("#one"), 2.0, { ease: Bounce.easeOut, x:150, opacity:1 });
         
         
        
         
     } else{
-        
-        TweenLite.to($animation_elements, 0.1, { ease: Power4.easeOut, x:-700 });
-         
-            TweenLite.to($("#two"), 0.1, { ease: Power4.easeOut, x:350});
+         TweenLite.to($(".example") ,2, {opacity:0});  
+        TweenLite.to($animation_elements, 0.1, { ease: Power4.easeOut, x:-700, opacity:0 });         
+            TweenLite.to($("#two"), 0.1, { ease: Power4.easeOut, x:350,opacity:0});
         
         
          
@@ -54,5 +56,49 @@ var $window = $(window);
     $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');     
         
+        
+         }else if(windowWidth <= 1249){
+             
+                         function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+              
+  TweenLite.to($("#one"), 2.0 , {x:0})
+  TweenLite.to($("#two"), 2.0 , {x:0})
+  TweenLite.to($(".example") , 1.0, {y:0 , delay:2.0 }  )
+        
+       
+        
+    } else{
+       
+         TweenLite.to($(".socials"), 1.0 , {x:450})
+           TweenLite.to($("#one"), 2.0 , {x:-950})
+           TweenLite.to($("#two"), 2.0 , {x:950})
+          TweenLite.to($(".example") , 1.0, { y:250})
+    }
+  });
+}  
+    $window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');     
+        
+         }
+     
+    
+   });
+    
+    $(window).scroll(check_if_in_view());
+     
+  
     
 });
